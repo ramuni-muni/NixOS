c0de5b2728668e46ec24870e0234d9c300607a14
ramuni-muni <100084739+ramuni-muni@users.noreply.github.com>
2023-08-18 21:26:50 +0700

    Update inkscape.nix

--- .config/nixpkgs/overlays/inkscape.nix
+++ .config/nixpkgs/overlays/inkscape.nix
@@ -1,11 +1,11 @@
 self: super: 
 {
-	inkscape = super.inkscape.overrideAttrs (prev: rec{
+	inkscape = super.inkscape.overrideAttrs (oldAttrs: rec{
 		version = "1.3";
 		src = super.fetchurl {	
        			url = "https://media.inkscape.org/dl/resources/file/inkscape-${version}.tar.xz";
         		sha256 = "sha256-v08oawJeAWm4lIzBTVGZqbTCBNdhyJTEtISWVx7HYwc=";
 		};
-		buildInputs = prev.buildInputs ++ [ super.double-conversion super.libepoxy ];
+		buildInputs = oldAttrs.buildInputs ++ [ super.double-conversion super.libepoxy ];
 	});    	
 }
