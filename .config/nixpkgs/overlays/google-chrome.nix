88b4b5b1d8831d8ca2ae34229686b2478265ad2b
ramuni-muni <100084739+ramuni-muni@users.noreply.github.com>
2023-11-10 22:13:14 +0700

    override and overrideAtrs for latest version

--- .config/nixpkgs/overlays/google-chrome.nix
+++ .config/nixpkgs/overlays/google-chrome.nix
@@ -1,6 +1,12 @@
 self: super: 
 {
-	google-chrome = super.google-chrome.override (prev: rec{
-		speechd = "";
-	});    	
+  google-chrome = (super.google-chrome.override (prev: rec{
+    speechd = "";		
+  })).overrideAttrs (oldAttrs: rec{
+    name = "google-chrome";		
+    src = super.fetchurl {	
+      url = "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb";
+      sha256 = "sha256-WLTTFMUvtBHvvegDFpZ+7Eht9StMyleaqXEBhPhgPTs=";
+    };
+  });    		
 }
